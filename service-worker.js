05a6ae615b2805e6bf3347b9ddd2553077d56db2
markhevia83 <markhevia@gmail.com>
2026-09-14 19:29:32 +0200

    Actualizar caché para distribuir la corrección del formulario

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "hipoteca-control-v1";
+const CACHE = "hipoteca-control-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
