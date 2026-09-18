16b49c47de691feb372b6b405e4eae173481c86b
markhevia83 <markhevia@gmail.com>
2026-09-18 11:11:49 +0200

    Aclarar el plazo y destacar el cálculo hipotecario

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "hipoteca-control-v6-plazo";
+const CACHE = "hipoteca-control-v7-calculo-visible";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./client-report.js?v=1", "./vendor/jspdf-4.2.1.umd.min.js"];
 self.addEventListener('message', event => {
   if (event.data === 'hc-team-cache-safe') event.ports[0]?.postMessage(true);
